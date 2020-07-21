fdd19acb47441b74ce3d8173fe190657b92ab147
amiruljbr <amiruljabar@gmail.com>
2020-07-21 11:26:42 +0700

    demo done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <click/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Click from './components/Click.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Click
   }
 }
 </script>
---
+++ src/store/main.js
@@ -0,0 +1,14 @@
+import { reactive, readonly } from 'vue';
+export class Store {
+    constructor() {
+        let data = this.data();
+        this.setup(data);
+        this.state = reactive(data);
+    }
+    setup(data) { }
+    getState() {
+        // TODO the readonly state should only be used in dev environment... otherwise just return the state
+        return readonly(this.state);
+    }
+}
+//# sourceMappingURL=main.js.map
